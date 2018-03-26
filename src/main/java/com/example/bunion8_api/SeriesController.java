bd6db59363ebf725eeeaebf5bb4979d04882a1e5
Aime <ndagijimana2@gmail.com>
2018-03-26 15:50:00 -0700

    Added controller

---
+++ src/main/java/com/example/bunion8_api/SeriesController.java
@@ -0,0 +1,44 @@
+package com.example.bunion8_api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SeriesController {
+	
+	
+	@Autowired
+	private SeriesInfoRepository seriesInfoRepository;
+	
+	@GetMapping("/series")
+	public List<SeriesInfo> getSeiesInfo(){
+		return seriesInfoRepository.findAll();
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		SeriesInfo newSeries = new SeriesInfo();
+		newSeries.setName("Three Stooges");
+		
+		Rating rating = new Rating();
+		rating.setStars(5);
+		
+		Comment comment = new Comment();
+		comment.setBody("yuk yuk yuk");
+		
+		rating.setSeriesInfo(newSeries);
+		comment.setSeriesInfo(newSeries);
+		
+		newSeries.getRatings().add(rating);
+        newSeries.getComments().add(comment);
+        
+        seriesInfoRepository.save(newSeries);
+        
+        return "test";
+		
+	}
+
+}
